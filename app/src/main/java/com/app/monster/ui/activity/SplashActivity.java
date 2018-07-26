3450d3abec0651e17954c6da03928d4094ae6754
llb <13917944029@163.com>
2018-07-26 10:17:22 +0800

    12

--- app/src/main/java/com/app/monster/ui/activity/SplashActivity.java
+++ app/src/main/java/com/app/monster/ui/activity/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.app.monster.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +9,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SplashActivity extends AppCompatActivity{
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
